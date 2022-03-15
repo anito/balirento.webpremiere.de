05c3ab0d770d7a623a641406543fffbe3c309bc5
anito <axelnitzschner@mac.com>
2022-03-15 18:57:48 +0100

    child

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,4 +1,19 @@
 <?php
 /**
- * Child functions and definitions.
+ * Define Constants
  */
+define('CHILD_THEME_ASTRA_CHILD_VERSION', '0.0.1');
+
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
+function child_enqueue_styles()
+{
+  wp_enqueue_style('astra-child-theme', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all');
+}
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);

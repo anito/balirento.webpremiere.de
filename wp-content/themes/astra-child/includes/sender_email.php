ac578251c61f8c1dee4ae3ddba14074ccd9168ef
anito <axelnitzschner@mac.com>
2022-03-23 05:36:49 +0100

    sender email

--- wp-content/themes/astra-child/includes/sender_email.php
+++ wp-content/themes/astra-child/includes/sender_email.php
@@ -18,6 +18,6 @@ add_filter('wp_mail_from', 'wpb_sender_email');
  */
 function wpb_sender_name($original_email_from)
 {
-    return 'Balirento | La Palma Ressort';
+    return 'La Palma Ressort';
 }
 add_filter('wp_mail_from_name', 'wpb_sender_name');
